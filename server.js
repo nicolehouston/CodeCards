b7ed0599070fc8ec32190cbd04b313be605362fd
Robert Cortez <Nuhuhfoo@gmail.com>
2018-07-13 12:47:00 -0700

    update

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -12,8 +11,6 @@ app.use(bodyParser.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-// Add routes, both API and view
-app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/CodeCards");
