8abf3bb4dfa2279bba7716542551876198e44cdd
Robert Cortez <Nuhuhfoo@gmail.com>
2018-07-24 20:33:15 -0700

    updates

--- client/src/components/WelcomeCard/WelcomeCard.js
+++ client/src/components/WelcomeCard/WelcomeCard.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import "./WelcomeCard.css";
 
 const styles = {
   card: {
@@ -24,20 +21,13 @@ function SimpleCard(props) {
   const { classes } = props;
 
   return (
-    <div>
-      <Card className={classes.card}>
-        <CardContent>
-          <Typography variant="headline" component="h2">
-            Welcome to &lt;MyCodeCard/&gt;!
-          </Typography>
-          <Typography component="p">
-            The site that helps you organize and keep track of your wealth of knowledge.
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button href="/login" size="small">Login</Button>
-        </CardActions>
-      </Card>
+    <div className={"buffer"}>
+      <div className={"typewriter"}><h1>&lt;MyCodeCard/&gt;</h1></div>
+      <div className={"intro"} variant="headline">Organized learning for a not so organized world.</div>
+              <div className={"introBtn"}>
+                <Button href="/login" variant="outlined" color="primary">Get Started
+                </Button>
+              </div>
     </div>
   );
 }
